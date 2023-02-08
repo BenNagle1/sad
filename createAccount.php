855ea3ab05678ca85b40b277a4c2ecc40933d17b
BenNagle1 <72495348+BenNagle1@users.noreply.github.com>
2023-02-08 16:13:32 +0000

    Add files via upload

--- createAccount.php
+++ createAccount.php
@@ -1,65 +1,27 @@
 <?php
 include "db.inc.php";
 
-session_start();
-
-$key = 'thebestsecretkey';
-
-function encrypt($input, $key) {
-    $encryption_key = base64_decode($key);
-    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-128-cbc'));
-    $encrypted = openssl_encrypt($input, 'aes-128-cbc', $encryption_key, 0, $iv);
-    return base64_encode($encrypted . '::' . $iv);
+function generateSalt($password) {
+    $salt = '';
+    for ($iteration = 0; $iteration < strlen($password); $iteration++) {
+        $salt .= chr(mt_rand(33, 126));
+    }
+    return $salt;
 }
 
-$username = "";
-$password = "";
-$confirm_Password = "";
-$username_err =  "";
-$password_err = "";
-$error = false;
-
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $username = mysqli_real_escape_string($db, $_POST['username']);
-    $password = mysqli_real_escape_string($db, $_POST['password']);
-    $confirm_Password = mysqli_real_escape_string($db, $_POST['confirmpassword']);
-
-    // Ensure that username does not already exist in database
-    $user_validate = "SELECT * FROM users WHERE username='$username' LIMIT 1";
-    $result = mysqli_query($db, $user_validate);
-    $user_check = mysqli_fetch_assoc($result);
-
-    if ($user_check) {
-        if ($user_check['username'] === $username) {
-            $error = true;
-            $username_err = "Username already exists in our records";
-        }
+function hash_password($password) {
+    $count = 10000;
+    $hash = hash('sha256', $password . $salt);
+    for ($i = 0; $i < $count; $i++) {
+        $hash = sha256($hash . $salt);
     }
-
-    if (strlen($password) < 8) {
-        $error = true;
-        $password_err = "Password must have at least 8 characters.";
-    }
-
-    if ($password != $confirm_Password) {
-        $password_err = "Passwords do not match";
-        $error = true;
-    }
-
-    if (!$error) {
-        $encrypted_username = encrypt($username, $key);
-        $encrypted_password = encrypt($password, $key);
-
-        $sql_query = "INSERT INTO users (`username`, `password`) 
-        VALUES ('$encrypted_username', '$encrypted_password')";
-
-        if (mysqli_query($db, $sql_query)) {
-            header("location: login.php");
-        }
-    }
-
-    mysqli_close($db);
+    return $hash;
 }
+
+$password = $_POST['password'];
+$salt = generateSalt(16);
+$hashedPassword = hash_password($password, $salt);
+
 ?>
  
 <!DOCTYPE html>
