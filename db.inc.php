3c2c68a7e839535dbf2d1a8dddcb1c07ec90209c
BenNagle1 <72495348+BenNagle1@users.noreply.github.com>
2023-03-14 15:57:29 +0000

    Add files via upload

--- db.inc.php
+++ db.inc.php
@@ -1,19 +1,22 @@
 <?php
-$db_name = "localhost";
+$db_hostname = "localhost";
 $db_username = "root";
 $db_password = "";
 $db_name = "sad";
 
-$db = mysqli_connect("localhost", $db_username, $db_password);
+$db = mysqli_connect($db_hostname, $db_username, $db_password);
 
 if (!$db) {
-    die("Connection to databse failed: " . mysqli_connect_error());
+    die("Connection to database failed: " . mysqli_connect_error());
 }
 
-if (!mysqli_select_db($db, $db_name)) {
-    $sql = "CREATE DATABASE $db_name";
-    
+// Create the database if it doesn't exist
+$create_db_sql = "CREATE DATABASE IF NOT EXISTS $db_name";
+if (!$db->query($create_db_sql)) {
+    die("Error creating database: " . $db->error);
 }
+
+mysqli_select_db($db, $db_name);
 
 $table_name = "users";
 $table_check = "SHOW TABLES LIKE '$table_name'";
@@ -27,7 +30,10 @@ if (mysqli_num_rows($table_result) == 0) {
         `hashed_password` varchar(66) NOT NULL,
         `isAdmin` tinyint(4) NOT NULL DEFAULT 0
       ) ";
-
+    if (mysqli_query($db, $table_create)) {
+        echo "Table created successfully";
+    } else {
+        die("Error creating table: " . mysqli_error($db));
+    }
 }
 
-?>
