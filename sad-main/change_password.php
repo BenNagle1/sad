c7ef336798404b17253f72adcbb91f1cae75a49b
BenNagle1 <72495348+BenNagle1@users.noreply.github.com>
2023-03-24 15:59:36 +0000

    Add files via upload

--- sad-main/change_password.php
+++ sad-main/change_password.php
@@ -1,14 +1,14 @@
 <?php
 session_start(); 
-include "db.inc.php";
-require_once 'functions.php';
+include "db.inc.php"; 
+require_once 'functions.php'; 
 
-if (!isset($_SESSION['username'])) {
+if (!isset($_SESSION['username'])) { // Chekcs if the user is logged in, if not redirect to login page
     header("Location: login.php");
     exit;
 }
 
-$username = "";
+$username = ""; 
 $password = "";
 $confirm_password = "";
 $username_err =  "";
@@ -17,49 +17,55 @@ $password_err2 = "";
 $password_err3 = "";
 $error = false;
 
-if (isset($_SESSION['user_id'])) {
+
+if (isset($_SESSION['user_id'])) { 
     $user_id = $_SESSION['user_id'];
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['change_password'])) {
-        if (isset($_POST['change_password'])) {
-            $current_password = mysqli_real_escape_string($db, $_POST['current_password']);
-            $new_password = mysqli_real_escape_string($db, $_POST['new_password']);
-            $confirm_password = mysqli_real_escape_string($db, $_POST['confirm_password']);
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['change_password'])) { // Check if HTTP request is POST and form has been submitted
+        if (isset($_POST['change_password'])) { // Check if the form form has been submitted (user clicks change password button)
+            $current_password = mysqli_real_escape_string($db, $_POST['current_password']); 
+            $new_password = mysqli_real_escape_string($db, $_POST['new_password']); 
+            $confirm_password = mysqli_real_escape_string($db, $_POST['confirm_password']); 
     
             $user_id = $_SESSION['user_id'];
-            $sql = "SELECT * FROM users WHERE id=? LIMIT 1";
-            $statement = mysqli_prepare($db, $sql);
+            //Select the user from the users table with the matching ID of logged in user 
+            $sql = "SELECT * FROM users WHERE id=? LIMIT 1"; 
+            $statement = mysqli_prepare($db, $sql); 
             mysqli_stmt_bind_param($statement, "i", $user_id);
-            mysqli_stmt_execute($statement);
-            $user_result = mysqli_stmt_get_result($statement);
-            $user = mysqli_fetch_assoc($user_result);
+            mysqli_stmt_execute($statement); 
+            $user_result = mysqli_stmt_get_result($statement); 
+            $user = mysqli_fetch_assoc($user_result); 
     
-            if (!$user || !verify_password($current_password, $user['hashed_password'], $user['salt'])) {
+            //Check if the entered old password matches the users table entry 
+            if (!$user || !verify_password($current_password, $user['hashed_password'], $user['salt'])) { 
                 $error = true;
                 $password_err1 = "Current password does not match our records";
             }
     
+            //Check new password is greater than 8 chars 
             if (strlen($new_password) < 8) {
                 $error = true;
                 $password_err2 = "Your password must have at least 8 characters.";
             }
     
-            if (trim($new_password) != trim($confirm_password)) {
-                $password_err3 = "The passwords you entered do not match.";
+            //Check if the new passwords match
+            if (trim($new_password) != trim($confirm_password)) { 
+                $password_err3 = "The new passwords you entered do not match.";
                 $error = true;
             }
     
-            if (!$error) {
+            if (!$error) { 
                 $salt_length = strlen($new_password);
-                $salt = generateSalt($salt_length);
-                $hashed_password = hash_password($new_password, $salt);
-                $hashed_password = mysqli_real_escape_string($db, $hashed_password);
-                $update_query = "UPDATE users SET salt=?, hashed_password=? WHERE id=?";
-                $update_statement= mysqli_prepare($db, $update_query);
-                mysqli_stmt_bind_param($update_statement, "ssi", $salt, $hashed_password, $user_id);
-                mysqli_stmt_execute($update_statement);
+                $salt = generateSalt($salt_length); 
+                $hashed_password = hash_password($new_password, $salt); 
+                $hashed_password = mysqli_real_escape_string($db, $hashed_password); 
+                // Now, update the user record with new salt and the new hashed password
+                $update_query = "UPDATE users SET salt=?, hashed_password=? WHERE id=?"; 
+                $update_statement= mysqli_prepare($db, $update_query); 
+                mysqli_stmt_bind_param($update_statement, "ssi", $salt, $hashed_password, $user_id); 
+                mysqli_stmt_execute($update_statement); 
     
-                if (mysqli_stmt_affected_rows($update_statement) > 0) {
-                    header("location: login.php");
+                if (mysqli_stmt_affected_rows($update_statement) > 0) { //Checkk if the update query affected at least one row. If not, displya the below error message. 
+                    header("location: login.php"); //redirect to login page 
                 } else {
                     echo "Error: " . $update_query . "<br>" . mysqli_error($db);
                 }
--- sad-main/tracker.php
+++ sad-main/tracker.php
@@ -1,24 +1,28 @@
 <?php 
-
-
+// Check if the user is logged in 
 if(isset($_SESSION['username'])) {
+    
+    // Get the current time 
     $time = time();
 
+    // Check if the users last active time has been set 
     if(isset($_SESSION['last_active'])){
 
+        // Calculate how many seconds have passed since the user was last acitve, this is the inactive time
         $inactiveTime = $time - $_SESSION['last_active'];
 
+        // If the inactive time exceeds 10 mins (600 seconds), the session is destroyed and the user is redirected to the login page
         if($inactiveTime > 600) {
-
             session_destroy();
             header('Location: login.php');
+
             exit();
 
         }
 
     }
 
-
+    // Last active time is updated to the current time if the user is still active 
     $_SESSION['last_active'] = $time;
 
 }
